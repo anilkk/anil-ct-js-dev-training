0053a4a096443898dd6f5f6a1acf1f25c7f97349
Anil Kumar <anilbms75@gmail.com>
2021-03-16 09:37:21 +0100

    task 4b cart to order

--- Task04b_checkout.js
+++ Task04b_checkout.js
@@ -3,31 +3,37 @@ const { log } = require("./logger.js");
 
 const cartDraftData = {
   currency: "EUR",
-  customerId: "10cb16bf-a5d8-4f47-b664-fe5cae2f75d0",
+  customerId: "91f8fb2e-1667-44f2-afc0-7e1c693bce3b",
   countryCode: "DE",
 };
+
 const paymentDraft = {
   key:"testPayment-2",
   amountPlanned:{
     currencyCode:'EUR',
-    centAmount:4200
+    centAmount:270000
   },
   customer:{
     typeId:'customer',
-    id:'10cb16bf-a5d8-4f47-b664-fe5cae2f75d0'
+    id:'91f8fb2e-1667-44f2-afc0-7e1c693bce3b'
   }
 }
-//checkout.createCart(cartDraftData).then(log).catch(log)
 
-//checkout.addLineItemsToCart(['123','123'],'294f2971-9497-43c1-8898-b7760082c842').then(log).catch(log)
+// checkout.createCart(cartDraftData).then(log).catch(log)
 
-//checkout.getCartById('294f2971-9497-43c1-8898-b7760082c842').then(log).catch(log)
+// checkout.addLineItemsToCart(['macbook-15-128','macbook-15'],'f2a94057-8709-4eca-bd1e-930d3d82116b').then(log).catch(log)
 
-//checkout.createOrderFromCart('294f2971-9497-43c1-8898-b7760082c842').then(log).catch(log)
+// checkout.getCartById('f2a94057-8709-4eca-bd1e-930d3d82116b').then(log).catch(log)
+// checkout.getCartById('f2a94057-8709-4eca-bd1e-930d3d82116b').then(cart => {
+//   log(cart.body.lineItems[0])
+// }).catch(log)
 
-//checkout.getOrderById('abaf987f-7be6-4f55-9323-cf8921f28075').then(log).catch(log)
-//checkout.createPayment(paymentDraft).then(log).catch(log)
-//checkout.addPaymentToOrder('bd051b5c-9cd4-4cfc-8536-bac669f579f4','abaf987f-7be6-4f55-9323-cf8921f28075').then(log).catch(log)
+// checkout.createOrderFromCart('f2a94057-8709-4eca-bd1e-930d3d82116b').then(log).catch(log)
+
+// checkout.getOrderById('37b49f01-b417-49c5-adc9-2df5ffdcf3f5').then(log).catch(log)
+// checkout.createPayment(paymentDraft).then(log).catch(log)
+// checkout.getPaymentById('d56dd0d9-e220-48b9-83f3-315b83358aaf').then(log).catch(log)
+checkout.addPaymentToOrder('d56dd0d9-e220-48b9-83f3-315b83358aaf','37b49f01-b417-49c5-adc9-2df5ffdcf3f5').then(log).catch(log)
 
 // checkout
 //   .updateOrderCustomState(
@@ -37,27 +43,27 @@ const paymentDraft = {
 //   .then(log)
 //   .catch(log);
 
-const checkoutProcess = async () => {
-  let emptyCart = await checkout.createCart(cartDraftData);
+// const checkoutProcess = async () => {
+//   let emptyCart = await checkout.createCart(cartDraftData);
 
-  let filledCart = await checkout.addLineItemsToCart(
-    ["123", "123",'123'],
-    emptyCart.body.id
-  );
-  filledCart = await checkout.addDiscountCodeToCart(
-    "SUMMER",
-    emptyCart.body.id
-  );
-  const payment = await checkout.createPayment(paymentDraft);
-  let order = await checkout.createOrderFromCart(filledCart.body.id);
-  order = await checkout.addPaymentToOrder(payment.body.id,order.body.id);
-  order = await checkout.setOrderState('Confirmed',order.body.id);
-  if (order) {
-    return {
-      status: 201,
-      message: "order created",
-    };
-  }
-};
+//   let filledCart = await checkout.addLineItemsToCart(
+//     ["123", "123",'123'],
+//     emptyCart.body.id
+//   );
+//   filledCart = await checkout.addDiscountCodeToCart(
+//     "SUMMER",
+//     emptyCart.body.id
+//   );
+//   const payment = await checkout.createPayment(paymentDraft);
+//   let order = await checkout.createOrderFromCart(filledCart.body.id);
+//   order = await checkout.addPaymentToOrder(payment.body.id,order.body.id);
+//   order = await checkout.setOrderState('Confirmed',order.body.id);
+//   if (order) {
+//     return {
+//       status: 201,
+//       message: "order created",
+//     };
+//   }
+// };
 
-checkoutProcess().then(log).catch(log);
+// checkoutProcess().then(log).catch(log);
--- handson/order.js
+++ handson/order.js
@@ -1,6 +1,13 @@
 const { apiRoot, projectKey } = require("./client.js");
 
-module.exports.createCart = (cartDraftData) =>{}
+module.exports.createCart = (cartDraftData) =>
+  apiRoot
+    .withProjectKey({ projectKey })
+    .carts()
+    .post({
+      body: createCartDraft(cartDraftData),
+    })
+    .execute();
 
 const createCartDraft = (cartDraftData) => {
   const { currency, customerId, countryCode } = cartDraftData;
@@ -14,18 +21,41 @@ const createCartDraft = (cartDraftData) => {
   };
 };
 
-module.exports.customerSignIn = (customerDetails) =>{}
+module.exports.customerSignIn = (customerDetails) => {};
 
-module.exports.getCartById = (ID) =>{}
+module.exports.getCartById = (ID) =>
+  apiRoot.withProjectKey({ projectKey }).carts().withId({ ID }).get().execute();
 
-module.exports.addLineItemsToCart = (arrayOfSKUs, cartId) => {}
+module.exports.addLineItemsToCart = (arrayOfSKUs, cartId) => {
+  return this.getCartById(cartId).then(cart => {
+    const updateActions = [];
+    arrayOfSKUs.forEach(sku => {
+      updateActions.push({
+        action: 'addLineItem',
+        sku
+      })
+    });
 
-module.exports.addDiscountCodeToCart = (discountCode, cartId) => {}
+    return apiRoot.withProjectKey({projectKey}).carts().withId({ID: cart.body.id}).post({
+      body: {
+        actions: updateActions,
+        version: cart.body.version
+      }
+    }).execute();
+  })
+}
+module.exports.addDiscountCodeToCart = (discountCode, cartId) => {};
 
-module.exports.createOrderFromCart = (cartId) => {}
+module.exports.createOrderFromCart = (cartId) => {
+  return createOrderFromCartDraft(cartId).then(cartDraftData => {
+    return apiRoot.withProjectKey({projectKey}).orders().post({
+      body: cartDraftData
+    }).execute();
+  })
+}
 
 const createOrderFromCartDraft = (cartId) => {
-  return getCartById(cartId).then((cart) => {
+  return this.getCartById(cartId).then((cart) => {
     return {
       id: cart.body.id,
       version: cart.body.version,
@@ -33,12 +63,33 @@ const createOrderFromCartDraft = (cartId) => {
   });
 };
 
-module.exports.getOrderById = (ID) =>{}
+module.exports.getOrderById = (ID) => apiRoot.withProjectKey({projectKey}).orders().withId({ID}).get().execute();
 
-module.exports.updateOrderCustomState = (customStateId, orderId) => {}
+module.exports.updateOrderCustomState = (customStateId, orderId) => {};
 
-module.exports.createPayment = (paymentDraft) =>{}
+module.exports.createPayment = (paymentDraft) => apiRoot.withProjectKey({projectKey}).payments().post({
+  body: paymentDraft
+}).execute();
 
-module.exports.setOrderState = (stateName, orderId) => {}
+module.exports.setOrderState = (stateName, orderId) => {};
+module.exports.getPaymentById = (ID) => apiRoot.withProjectKey({projectKey}).payments().withId({ID}).get().execute();
 
-module.exports.addPaymentToOrder = (paymentId, orderId) => {}
+module.exports.addPaymentToOrder = (paymentId, orderId) => {
+  return this.getOrderById(orderId).then(order => {
+      const payment = '';
+      const updateActions = [{
+        action: 'addPayment',
+        payment: {
+          "typeId": "payment",
+          "id": paymentId
+        }
+      }];
+
+      return apiRoot.withProjectKey({projectKey}).orders().withId({ID: order.body.id}).post({
+        body: {
+          actions: updateActions,
+          version: order.body.version
+        }
+      }).execute();
+  });
+};
